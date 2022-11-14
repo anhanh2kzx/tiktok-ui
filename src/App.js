46b946a43b48e777eaf19339306b7de93daf7cb5
anhld <anhld69@fpt.com.vn>
2022-11-14 22:32:07 +0700

    Dựng khung layout mặc định

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Page = route.component;
+                    const Layout = route.layout || DefaultLayout;
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </Router>
     );
 }
 
